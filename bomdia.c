7d26cb1ae6e5034f875e8ca3eb83dc0f46078c6f
JTXL15 <joaotallysonn@gmail.com>
2025-09-28 12:04:57 -0300

    versao final pré-apresentacao 3.0

--- bomdia.c
+++ bomdia.c
@@ -3,10 +3,32 @@
 #include <stdlib.h>
 
 // função que mostra a documentação / os comandos utilizados no sistema
+void mostrar_documentacao();
+// função que mostra as mensagens de acordo com o código indicado
+void mostrar_bomdia(const char *comando);
+void mostrar_dica();
+void mostrar_error();
+void mostrar_error2();
+
+int main(int argc , char const *argv[]) {
+
+    if(argc == 2){
+        mostrar_bomdia(argv[1]);
+    }
+
+    else if(argc == 1){
+        mostrar_error();
+    }
+
+    else{
+        mostrar_error2();
+    }
+    return 0;
+}
 
 void mostrar_documentacao(){
 
-     printf("LISTA DE COMANDOS:\n\n");
+     printf("\n\nLISTA DE COMANDOS:\n\n");
 
     printf("--help ou -h(documentação do software)\n");
 
@@ -33,42 +55,44 @@ void mostrar_dica(){
     printf("Utilize o comando bomdia -h ou bomdia --help para acessar o menu de ajuda!\n\n");
 }
 
-// função que mostra as mensagens de acordo com o código indicado
-void mostrar_message(const char *comando){
+
+void mostrar_bomdia(const char *comando){
     if(strcmp(comando , "--pt") == 0 || strcmp(comando , "-1") == 0){
-        printf("Bom Dia!\n");
+        printf("\n\nBom Dia!\n");
     }
 
     else if(strcmp(comando , "--en") == 0 || strcmp (comando , "-2") == 0){
-        printf("Good Morning!\n");
+        printf("\n\nGood Morning!\n");
     }
 
     else if(strcmp(comando , "--es") == 0 || strcmp (comando , "-3") == 0){
-        printf("Buenos dias!\n");
+        printf("\n\nBuenos días!\n");
     }
 
     else if(strcmp(comando , "--fr") == 0 || strcmp (comando , "-4") == 0){
-        printf("Bonjour!\n");
+        printf("\n\nBonjour !\n");
     }
 
     else if(strcmp(comando , "--it") == 0 || strcmp (comando , "-5") == 0){
-        printf("Buongiorno!\n");
+        printf("\n\nBuongiorno!\n");
     }
 
     else if(strcmp(comando , "--co") == 0 || strcmp (comando , "-6") == 0){
-        printf("좋은 아침이에요!\n");
+        printf("\n\n좋은 아침이에요!\n");
     }
 
     else if(strcmp(comando , "--ja") == 0 || strcmp (comando , "-7") == 0){
-        printf("おはよう!\n");
+        printf("\n\nおはよう!\n");
     }
 
     else if(strcmp(comando , "--ru") == 0 || strcmp (comando , "-8") == 0){
-        printf("Доброе утро!\n");
+        printf("\n\nДоброе утро!\n");
     }
 
     else if(strcmp(comando , "--version") == 0 || strcmp (comando , "-v") == 0){
-        printf("\n| Versao : bomdia 1.0|\n\nBy: Tallyson Aguiar\n");
+        printf("\n\nbomdia 3.0\n");
+        printf("Autor: Tallyson Aguiar – Ciências da Computação, CCET-UVA\n");
+        printf("Este software é de uso livre para fins acadêmicos.\n");
     }
 
     else if(strcmp(comando , "--help") == 0 || strcmp (comando , "-h") == 0){
@@ -76,6 +100,7 @@ void mostrar_message(const char *comando){
     }
 
     else{
+        printf("\n\n|%s não é um comando válido|\n\n" , comando);
         mostrar_dica();
     }
 
@@ -91,21 +116,4 @@ void mostrar_error(){
 void mostrar_error2(){
     printf("\n\n  / Erro: Mais de uma opção informada! /\n\n");
     mostrar_dica();
-}
-
-
-int main(int argc , char const *argv[]) {
-
-    if(argc == 2){
-        mostrar_message(argv[1]);
-    }
-
-    if(argc == 1){
-        mostrar_error();
-    }
-
-    if(argc > 2){
-        mostrar_error2();
-    }
-    return 0;
 }
